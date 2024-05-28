f17d971c8f90fdb3e50d6342334076d7598b5787
Deep Goyal <deep.goyal@icloud.com>
2024-05-28 22:53:02 +0530

    add navheading

--- app/page.js
+++ app/page.js
@@ -1,18 +1,17 @@
 import NavBar from "../components/NavBar";
-import Head from "next/head";
+import { FaChevronDown } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white dark:bg-white dark:text-black">
-      <Head>
-        <title>Deep Goyal Portfolio</title>
-        <meta name="description" content="Portfolio of Deep Goyal" />
-      </Head>
       <NavBar />
-      <main className="flex flex-col items-center justify-center min-h-screen">
+      <main className="flex flex-col items-center justify-center min-h-screen pb-40">
         <div className="flex flex-col items-center justify-center h-full">
           <h1 className="text-8xl font-bold mb-4">Hi! I'm Deep.</h1>
-          <h2 className="text-8xl">I'm a software engineer.</h2>
+          <h2 className="text-8xl mb-20">I'm a software engineer.</h2>
+        </div>
+        <div className="absolute bottom-0 w-full flex justify-center pb-10 ">
+          <FaChevronDown />
         </div>
       </main>
     </div>
--- components/NavBar.js
+++ components/NavBar.js
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { FiMoon } from "react-icons/fi";
 import { GoSun } from "react-icons/go";
+import { MdArrowOutward } from "react-icons/md";
 
 const NavBar = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -16,24 +17,32 @@ const NavBar = () => {
     }
   };
   return (
-    <nav className="flex justify-end items-center pt-10 pb-0 px-0 bg-gray-000 text-white dark:bg-white dark:text-black">
+    <nav className="flex justify-between items-center pt-10 pb-0 px-0 bg-gray-000 text-white dark:bg-white dark:text-black">
+      <h1 className="px-10 text-8xl">deep.</h1>
       <div className="flex justify-end">
-        <a href="mailto:deep.goyal@icloud.com" className="mr-4">
-          email ↗
+        <a
+          href="mailto:deep.goyal@icloud.com"
+          className="mr-4 flex items-center"
+        >
+          email <MdArrowOutward />
         </a>
         <a
           href="https://github.com/deep-goyal"
           target="_blank"
-          className="mr-4"
+          className="mr-4 flex items-center"
         >
-          github ↗
+          github <MdArrowOutward />
         </a>
-        <a href="/resume.pdf" target="_blank">
-          resume ↗
+        <a
+          href="/resume.pdf"
+          target="_blank"
+          className="mr-4 flex items-center"
+        >
+          resume {<MdArrowOutward />}
         </a>
         <button
           onClick={toggleDarkMode}
-          className="focus:outline-none px-5 pr-20"
+          className="focus:outline-none px-4 pr-20"
         >
           {darkMode ? <FiMoon size={25} /> : <GoSun size={25} />}
         </button>
