af2a007d50e8b788062054a00273e45e9e6fad50
Deep Goyal <deep.goyal@icloud.com>
2024-06-11 21:41:01 +0530

    add mobile breakpoints

--- app/components/MobileMessage.jsx
+++ app/components/MobileMessage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const MobileMessage = () => {
   return (
-    <div>
-      <h1>Coming Soon for Mobiles!</h1>
-      <p>This feature is currently under development.</p>
+    <div className="flex flex-col justify-center h-screen px-10">
+      <h1 className="text-3xl py-5 self-center">Coming Soon for Mobiles!</h1>
+      <p>This website is currently only available for larger screens</p>
       <p>
-        Please visit this website on larger devices such as laptops. I am
-        working tirelessly to make the experience equally magical on mobiles!
+        Please visit this website on larger devices like laptops. I am working
+        tirelessly to make the experience equally magical for mobile users!
       </p>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import MobileMessage from "./components/MobileMessage";
+
 import Aboutme from "@/app/components/Aboutme";
 import NavBar from "@/app/components/NavBar";
 
@@ -8,35 +12,59 @@ import Experience from "@/app/components/Experience";
 import Projects from "@/app/components/Projects";
 import Toolbar from "@/app/components/Toolbar";
 
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
-    <div className="min-h-screen bg-black text-white dark:bg-white dark:text-black">
-      <CanvasBackground />
-      <NavBar />
+    <>
+      {isMobile ? (
+        <MobileMessage />
+      ) : (
+        <div className="min-h-screen bg-black text-white dark:bg-white dark:text-black">
+          <CanvasBackground />
+          <NavBar />
 
-      <main className="flex flex-col items-center justify-center min-h-screen pb-0">
-        <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-8xl font-bold mb-4">Hi! I'm Deep.</h1>
-          <h2 className="text-8xl mb-20">I'm a Junior at ASU.</h2>
+          <main className="flex flex-col items-center justify-center min-h-screen pb-0">
+            <div className="flex flex-col items-center justify-center h-full">
+              <h1 className="text-8xl font-bold mb-4">Hi! I'm Deep.</h1>
+              <h2 className="text-8xl mb-20">I'm a Junior at ASU.</h2>
+            </div>
+          </main>
+          <div id="aboutme">
+            <Aboutme />
+          </div>
+          <div id="education">
+            <Education />
+          </div>
+          <div id="skills">
+            <Skills />
+          </div>
+          <div id="experience">
+            <Experience />
+          </div>
+          <div id="projects">
+            <Projects />
+          </div>
+
+          <Toolbar />
         </div>
-      </main>
-      <div id="aboutme">
-        <Aboutme />
-      </div>
-      <div id="education">
-        <Education />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="experience">
-        <Experience />
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-
-      <Toolbar />
-    </div>
+      )}
+    </>
   );
 }
