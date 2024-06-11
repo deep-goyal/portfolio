a083830fe86ada6db29a8d3c3fd4e7d6c55843b8
Deep Goyal <deep.goyal@icloud.com>
2024-06-11 21:26:50 +0530

    add mobile component

---
+++ app/components/MobileMessage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MobileMessage = () => {
+  return (
+    <div>
+      <h1>Coming Soon for Mobiles!</h1>
+      <p>This feature is currently under development.</p>
+      <p>
+        Please visit this website on larger devices such as laptops. I am
+        working tirelessly to make the experience equally magical on mobiles!
+      </p>
+    </div>
+  );
+};
+
+export default MobileMessage;
