e14f811ef4e200b6c743e7ef754a6c8d83aab67f
Deep Goyal <deep.goyal@icloud.com>
2024-07-04 22:11:37 +0530

    add navbar for pages

---
+++ components/PageNav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+import Deep from "@/public/deep.png";
+import {
+  RiHomeLine,
+  RiArrowLeftSLine,
+  RiArrowRightSLine,
+} from "react-icons/ri";
+
+const PageNav = () => {
+  return (
+    <div className="flex max-w-screen-xl flex-wrap flex-1 justify-between items-center pt-10 pl-40 px-0 mx-0">
+      <Image src={Deep} alt="Deep" width={100} height={100} />
+      <div className="flex justify-between items-center gap-8 text-xl">
+        <div
+          className="flex items-center  hover:text-gray-800"
+          onMouseEnter={() => document.body.classList.add("no-cursor")}
+          onMouseLeave={() => document.body.classList.remove("no-cursor")}
+        >
+          <RiArrowLeftSLine />
+          <p>prev</p>
+        </div>
+
+        <RiHomeLine
+          className=" hover:text-gray-800"
+          onMouseEnter={() => document.body.classList.add("no-cursor")}
+          onMouseLeave={() => document.body.classList.remove("no-cursor")}
+        />
+        <div
+          className="flex items-center  hover:text-gray-800"
+          onMouseEnter={() => document.body.classList.add("no-cursor")}
+          onMouseLeave={() => document.body.classList.remove("no-cursor")}
+        >
+          <p>next</p>
+          <RiArrowRightSLine />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNav;
