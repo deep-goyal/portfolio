d5f35f4877e313f221239c9a4ada080898631e6a
Deep Goyal <deep.goyal@icloud.com>
2024-07-12 19:08:56 +0530

    add favicon for bookmarks

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Deep from "@/public/nocurveblue.png";
 
 export const metadata = {
   title: "Deep Goyal",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Goudy+Bookletter+1911:wght@200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <link href={Deep.src} rel="icon" sizes="128x32" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
